c8cd928958b53e407babc7d289f22c4882e9dc4f
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-09-01 12:31:55 +0530

    [Suraj] added Encapsulation Examples

---
+++ OOPS/com/encapsulation/StudentMain.java
@@ -0,0 +1,12 @@
+package OOPS.com.encapsulation;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		StudentService service=new StudentService();
+		service.setStudentDetails();
+		service.displayStudentDetails();
+
+	}
+
+}
