1a7632e2df209fd2ddc50b5822767467a869a5a4
SURAJ NAGAICH <suraj.nagaich_cs23@gla.ac.in>
2025-09-04 22:25:18 +0530

    Delete OOPS directory

--- OOPS/com/Inheritence/MultiLevel_Inheritence/Parent.java
+++
@@ -1,9 +0,0 @@
-package OOPS.com.Inheritence.MultiLevel_Inheritence;
-
-public class Parent {
- public void multiply(int a,int b) {
-	 System.out.println(a*b);
-	 
- }
- 
-}
--- OOPS/com/Inheritence/Multiple_Inheritence/Parent1.java
+++
@@ -1,9 +0,0 @@
-package OOPS.com.Inheritence.Multiple_Inheritence;
-
-public class Parent1 {
-	public void add(int a,int b)
-	{
-		System.out.println(a+b);
-	}
-
-}
